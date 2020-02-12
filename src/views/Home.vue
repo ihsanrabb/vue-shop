e2ddce7592b96578aa56f63df8fc7d4bdce78709
ihsanrabb <ihsanrabbani25@gmail.com>
2020-02-12 22:04:44 +0700

    display product in front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,22 @@
       <h1 class="display-4">Sementara jumbotron</h1>
       <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
+    <ProductList />
     </div>
     <Login />
+    
   </div>
 </template>
 
 <script>
 import Login from "@/components/Login.vue";
+import ProductList from "../sections/ProductList.vue"
 
 export default {
   name: "home",
   components: {
-    Login
+    Login,
+    ProductList
   }
 };
 </script>
