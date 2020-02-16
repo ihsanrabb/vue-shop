b5a0f0643b4c8ddd9e59223b4415e5571f78f126
ihsanrabb <ihsanrabbani25@gmail.com>
2020-02-16 14:31:05 +0700

    create add to cart button

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="add-to-cart">
+    <button class="btn btn-primary">Beli bos</button>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "add-to-cart",
+  props: {
+    name: String,
+    price: String,
+    productId: String
+
+  },
+  data() {
+      return {
+          productName: this.name,
+          productPrice: this.price,
+          product_Id: this.productId
+      }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
