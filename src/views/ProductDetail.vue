23e89d98fa4a418df4003f82391247e8a67e4788
ihsanrabb <ihsanrabbani25@gmail.com>
2020-03-07 17:20:28 +0700

    create product detail

---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <Navbar />
+            <carousel :per-page="1"  :mouse-drag="false">
+                <slide v-for="(image,index) in products.images" :key="index">
+                    <img :src="image" />
+                </slide>
+            </carousel>
+            <p class="card-text" v-html="products.description" />
+            <AddToCart
+                :name="products.name"
+                :price="products.price"
+                :product-id="products.id"
+                :image="getImage(products.images)"
+            >
+            </AddToCart>
+            <button @click="beli" >nih beli</button>
+            <MiniCart />
+    </div>
+</template>
+
+<script>
+import {fb,db} from '../firebase';
+import { Carousel, Slide } from 'vue-carousel';
+import AddToCart from '../components/AddToCart';
+
+
+export default {
+    components: {
+        Carousel,
+        Slide,
+        AddToCart
+    },
+    data() {
+        return {
+            products: []
+        }
+    },
+    firestore() {
+        let prodId = this.$route.query.pId
+
+        return {
+            products: db.collection('products').doc(prodId)
+        }
+    },
+    methods: {
+        getImage(images) {
+            return images[0]
+        },
+        getProd() {
+            console.log('beli nih')
+        },
+        beli() {
+            let item = {
+                productName: this.products.name,
+                productPrice: this.products.price,
+                product_Id: this.products.id,
+                productImage: this.products.images,
+                productQuantity: 1
+            }
+            $('#miniCart').modal('show')
+            console.log('nih beli', item)
+            this.$store.commit('addToCart', item)
+        }
+    }
+}
+</script>
