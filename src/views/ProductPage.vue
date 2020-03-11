8f0c504479c281d8aba5d8e03945391d2d8c6abe
ihsanrabb <ihsanrabbani25@gmail.com>
2020-03-11 23:38:26 +0700

    create product page

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,10 +1,46 @@
 <template>
-    <div class="container-fluid">
+    <div class="">
             <Navbar />
-            <h1> ini product page</h1>
+            
+            <div class="parallax-makkah">
+                <h1>nyontek tokopedia, liat yang busana muslim</h1>
+            </div>
+            <ProductList />
+
     </div>    
 </template>
 
 <script>
+import ProductList from "../sections/ProductList.vue"
+import Parallax from 'vue-parallaxy'
 
+export default {
+  name: "productPage",
+  components: {
+      ProductList,
+      Parallax
+  },
+  data() {
+      return {
+          filter: 'All'
+      }
+  },
+  methods: {
+      setFilter(filterKey) {
+          this.filter = filterKey
+          console.log('set filter', this.filter)
+      }
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+    .parallax-makkah {
+        height: 250px;
+        background-image: url("../assets/img/Islamic-vector.svg");
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
