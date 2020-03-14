7a8fe4cdbc5c0869914c0bfab00bfc07eb696ab4
ihsanrabb <ihsanrabbani25@gmail.com>
2020-03-14 23:14:52 +0700

    checkout page

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,21 +1,85 @@
 <template>
   <div class="checkout">
     <Navbar />
-    <div class="container pt-5 mt-5">
-      <div class="row">
-          <div class="col-md-9">
-            <h4>Checkout page</h4>
-             <ul v-for="item in this.$store.state.cart">
-                <img :src="item.productImage" width="80px"  class="align-self-center" />
-                <li>{{item.productName}}</li>
-                <p class="mt-0">{{item.productPrice | currency('Rp') }}</p>
-                <p class="mt-0">Quantity : {{item.productQuantity}}</p>
-             </ul>
-          </div>
-          <div class="col-md-3">
-            Total Price
-          </div>
+    <div class="container">
+
+      <h1 class="checkout-title mt-5">Keranjang Belanja</h1>
+
+       <div v-if="productCount == 0">
+          <img src="../assets/img/empty-illus.jpg" class="img-empty">
+        </div>
+
+      <div v-else>
+        <div class="row">
+            <div class="col-md-8">
+              <div v-for="item in this.$store.state.cart">
+                  <div class="row product-detail">
+                    <div class="col-md-4">
+                      <img :src="item.productImage" width="80px"  class="align-self-center product-image" />
+                    </div>
+                    <div class="col-md-8">
+                      <p class="product-title">{{item.productName}}</p>
+                      <p class="mt-0 product-price">{{item.productPrice | currency('Rp') }}</p>
+                      <p class="mt-0">Jumlah beli : {{item.productQuantity}}</p>
+                      <img src="../assets/img/trash-can.svg" class="trash-icon float-right" @click="$store.commit('removeFromCart', item)">
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="price-container mt-4">
+                  <h2>Ringkasan belanja</h2> 
+                  <hr>
+                  <p class="mt-0">Total Harga :  <span class="total-price">{{totalPrice | currency('Rp') }}</span></p>
+                  <button type="button" class="btn btn-info">Beli ({{productCount}})</button>
+                  <button type="button" class="btn btn-outline-dark" @click="toShop">Lanjutkan Belanja!</button>
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import {fb} from '../firebase';
+
+export default {
+  data() {
+    return {
+      totalPrice: null,
+      productCount: null
+    }
+  },
+  methods: {
+    toShop() {
+      this.$router.push({path: '/productPage'})
+    }
+  },
+  created() {
+     let user = fb.auth().currentUser;
+      if (user) {
+        console.log('ada', user.email)
+      } else {
+        console.log('gaada')
+      }
+    const keranjang = JSON.parse(window.localStorage.getItem('cart'));
+    this.productCount = keranjang.length
+    let sum = []
+    if ( keranjang !== null ){
+      for (let i = 0; i < keranjang.length; i++) {
+        let harga = keranjang[i].productPrice
+        let total = keranjang[i].productQuantity
+        let totalProduct = harga * total
+        sum.push(totalProduct) 
+      }
+      
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      this.totalPrice = sum.reduce(reducer);
+    }
+   
+  }
+}
+</script>
+
+<style scoped lang="scss" src="../styles/Checkout.scss">
