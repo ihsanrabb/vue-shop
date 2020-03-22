fac81601400c3bc6a0cadd8cd52f4c1da3f19155
ihsanrabb <ihsanrabbani25@gmail.com>
2020-03-22 15:46:15 +0700

    create checkout finish page

---
+++ src/views/CheckoutFinish.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Navbar />
+
+        <h1>Terima kasih pesanan anda sedang dalam proses</h1>
+        <img src="../assets/svg/order-process.svg" style="width: 15%" />
+        <button type="button" class="btn btn-info">Kembali ke home</button>
+    </div>
+</template>
