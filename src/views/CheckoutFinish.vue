f9bdf82b662ed1285dc1f567fe23a6f12dbf1fb0
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-01 23:02:48 +0700

    force reload page

--- src/views/CheckoutFinish.vue
+++ src/views/CheckoutFinish.vue
@@ -5,5 +5,8 @@
         <h1>Terima kasih pesanan anda sedang dalam proses</h1>
         <img src="../assets/svg/order-process.svg" style="width: 15%" />
         <button type="button" class="btn btn-info">Kembali ke home</button>
+        <a href="/">
+        <p>Kembali ke home</p>
+        </a>
     </div>
 </template>
