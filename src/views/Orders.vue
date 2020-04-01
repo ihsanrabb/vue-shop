70d1f7e72d347c4c8589e7c35a314dc0d521e437
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-01 23:03:15 +0700

    create order page&modal detail

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -16,7 +16,7 @@
         <hr>
 
         <h3 class="d-inline-block">Order List</h3>
-          <button @click="seeOrder()" class="btn btn-primary float-right">See order</button>
+          
 
         <div class="product-test">
       
@@ -25,8 +25,8 @@
             <thead>
               <tr>
                 <th>Name</th>
-                <th>Price</th>
-                <th>MOdify</th>
+                <th>Order ID</th>
+                <th>Modify</th>
               </tr>
             </thead>
 
@@ -37,12 +37,11 @@
                 </td>
 
                 <td>
-                  {{product.price}}
+                  {{order.order_id}}
                 </td>
 
                 <td>
-                  <button @click="editProduct(product)" class="btn btn-primary">Edit</button>
-                  <button @click="deleteProduct(product)" class="btn btn-danger">Delete</button>
+                  <button @click="detailOrder(order)" class="btn btn-primary">Detail</button>
                 </td>
 
               </tr>
@@ -54,86 +53,46 @@
        </div>
     </div>
 
-    <!-- Modal -->
-      <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Edit Product</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
+    <div v-if="order.no_resi !== null">
+    <!-- Modal detail -->
+        <div class="modal" id="detailOrder" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title text-center">Detail pesanan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body detail-order">
+                <p>Nama Product : <span>{{order.product.productName}}</span></p>
+                <p>Jumlah yang dibeli : <span>{{order.product.productQuantity}}</span></p>
+                <p>Nama pembeli : <span>{{order.nama}}</span></p>
+                <p>Nomer telepon : <span>{{order.noPhone}}</span></p>
+                <p>Email : <span>{{order.email}}</span></p>
+                <p>Alamat : <span>{{order.alamat}}</span></p>
+                <p>Kode Pos : <span>{{order.kodePos}}</span></p>
+                <p>Tanggal Pembelian : <span>{{order.createdAt}}</span></p>
 
-              <div class="row">
-                  <!-- main product -->
-                  <div class="col-md-8">
-                    <div class="form-group">
-                      <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <vue-editor v-model="product.description"></vue-editor>
-                      <!-- <textarea placeholder="Product Description" class="form-control" v-model="product.description"></textarea> -->
-                    </div>
-                  </div>
-                  <!-- product sidebar -->
-                  <div class="col-md-4">
-                    <h4 class="display-6">Product Details</h4>
-                    <hr>
-
-                    <div class="form-group">
-                      <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                      <input type="text" @keyup.188="addTag" placeholder="Product tags" v-model="tag" class="form-control">
-                      
-                      <div  class="d-flex">
-                        <p v-for="tag in product.tags">
-                            <span class="p-1">{{tag}}</span>
-                        </p>
-                      </div>
-                    </div>
-                    </div>
-
-                    <div class="form-group">
-                      <label for="product_image">Product Images</label>
-                      <input type="file" @change="uploadImage" class="form-control">
-
-                    </div>
-
-                    <div class="form-group">
-                      <div class="p-1 d-flex">
-                          <div class="img-wrapp" v-for="(image, index) in product.images">
-                              <img :src="image" alt="" width="80px">
-                              <span class="delete-img" @click="deleteImage(image,index)">X</span>
-                          </div>
-                      </div>
-                    </div>
-
+                <div class="form-group mt-5">
+                 <p>NO RESI</p>
+                  <input type="email" class="form-control" id="no_resi" v-model="order.no_resi" >
                 </div>
-      <!-- end modal body -->
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button 
-                @click="addProduct()" 
-                type="button" 
-                class="btn btn-primary"
-                v-if="modal == 'new'" >
-                Save changes</button>
-              <button 
-                @click="updateProduct()"
-                type="button" 
-                class="btn btn-primary" 
-                v-if="modal == 'edit'"
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button 
+                  type="button" 
+                  class="btn btn-primary"
+                  @click="updateOrder()"  
                 >Apply changes</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      <!-- end modal -->
+    </div>
 
   </div>
 </template>
@@ -162,119 +121,41 @@ export default {
       modal: null,
       tag: null,
       orders: [],
-      hasilProduct: []
+      hasilProduct: [],
+      order: {
+        no_resi : null
+      }
     }
   },
   firestore() {
     const user = fb.auth().currentUser;
     console.log('firestore', user.uid)
     return {
-        orders: db.collection('orders').where("product.penjual_id", "==", user.uid)
+        orders: db.collection('orders').where("product.penjual_id", "==", user.uid),
+        orderan : db.collection('orders')
     }
   },
   methods: {
-    addNew() {
-      this.modal = 'new';
-      this.reset();
-      $('#product').modal('show');
+    detailOrder(order) {
+      this.order = order
+      $('#detailOrder').modal('show');
     },
-    deleteProduct(doc) {
-
-
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.value) {
-          this.$firestore.products.doc(doc.id).delete()
-          Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
-        }
-      })
-      
-    },
-    readData() {
-      
-    },
-    addProduct() {
-      console.log("ini hasil product : " + this.product);
-      this.$firestore.products.add(this.product);
-      $('#product').modal('hide');
-
-      Toast.fire({
-        icon: 'success',
-        title: 'Add successfully'
-      })
-    },
-    reset() {
-      this.product = {
-        name: null,
-        description: null,
-        price: null,
-        tags: [],
-        images: []
+    updateOrder() {
+      const data = {
+        no_resi : this.order.no_resi
       }
+
+      this.$firestore.orderan.doc(this.order.id).update(data)
+        .then(()=> {
+          $('#detailOrder').modal('hide');
+          Toast.fire({
+            icon: 'success',
+            title: 'No resi berhasil di update'
+          })
+        })
+        .catch((err)=> console.log(err));
+
     },
-    editProduct(product) {
-      this.modal = 'edit'
-      this.product = product;
-      $('#product').modal('show');
-      
-    },
-    updateProduct() {
-      this.$firestore.products.doc(this.product.id).update(this.product);
-
-      Toast.fire({
-        icon: 'success',
-        title: 'update successfully'
-      })
-
-      $('#product').modal('hide');
-    },
-    addTag() {
-      this.product.tags.push(this.tag);
-      this.tag = "";
-    },
-    uploadImage(e) {
-      if (e.target.files[0]) {
-        let file = e.target.files[0];
-
-        var storageRef = fb.storage().ref('products/' + file.name);
-
-        let uploadTask = storageRef.put(file);
-      
-        // console.log(e.target.files[0])
-
-        uploadTask.on('state_changed', (snapshot) => {  
-
-        }, (error) => {
-
-        },() => {
-          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-            this.product.images.push(downloadURL);
-            console.log('File available at', downloadURL);  
-          });
-        });
-      }
-    },
-    deleteImage(img,index) {
-      let deleteImg = fb.storage().refFromURL(img);
-
-      this.product.images.splice(index,1);
-      deleteImg.delete().then(function() {
-        console.log("image delete")
-      }).catch(function(error) {
-        console.log("error delete image")
-      })
-    }
       
   },
   mounted() {
@@ -285,18 +166,4 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-.img-wrapp {
-  position: relative;
-}
-
-.img-wrapp span.delete-img {
-  position: absolute;
-  top: -14px;
-  left: -2px;
-}
-
-.img-wrapp span.delete-img:hover {
-    cursor: pointer;
-}
-</style>
+<style scoped lang="scss" src="../styles/OrderAdmin.scss">
