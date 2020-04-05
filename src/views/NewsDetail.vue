7246fcb8c056a62a56ac021ffe588ecbc07b323d
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-05 18:18:01 +0700

    create newsDetail page

---
+++ src/views/NewsDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <Navbar />
+
+        <div class="container">
+            <div class="box-detail">
+                <img :src="newsDetail.imgInfo" class="img-fluid" alt="...">
+                <h5 class="card-title judul-news">{{newsDetail.judulInfo}}</h5>
+                <p class="card-text"><small class="text-muted">{{newsDetail.tglInfo}}</small></p>
+                <span v-html="newsDetail.deskripsiInfo"></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {fb,db} from '../firebase';
+
+export default {
+    name: "newsDetail",
+    data() {
+        return {
+            newsDetail : null
+        }
+    },
+    firestore() {
+        let infoId = this.$route.query.infoId
+
+        return {
+            newsDetail: db.collection('informasi').doc(infoId)
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss" src="../styles/NewsDetail.scss">
