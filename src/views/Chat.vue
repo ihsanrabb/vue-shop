ec3f48bb9c23f06d11f28f10a31aa9cda41b948c
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-07 22:45:05 +0700

    set value chat&query messages

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="container chat">
-        <h2 class="text-primary text-center">Real-time chat</h2>
-        <div class="card">
-            <div class="card-body">
-                <p class="text-secondary nomessage" v-if="messages.length == 0">
-                    [No messages yet!]
-                </p>
-                <div class="messages" v-chat-scroll="{always: false, smooth: true}">
-                    <div v-for="message in messages" :key="message.id">
-                        <span v-if="message.name == 'Admin'" class="text-danger">[{{ message.name }}]</span>
-                        <span v-else class="text-info">[{{ message.name }}]</span>
-                        <span>{{message.message}}</span>
-                        <span class="text-secondary time">{{message.timestamp}}</span>
+    <div>
+        <Navbar />
+        <div class="container chat mt-4">
+
+            <div v-if="userChat.isMessage == false">
+                <button class="btn btn-primary" @click="startChat">Mulai Chat!</button>
+            </div>
+
+            <div v-else>
+                <button class="btn btn-danger">End Chat</button>
+                <h2 class="text-primary text-center">Real-time chat</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="text-secondary nomessage" v-if="messages.length == 0">
+                            [No messages yet!]
+                        </p>
+                        <div class="messages" v-chat-scroll="{always: false, smooth: true}">
+                            <div v-for="message in messages" :key="message.id">
+                                <span v-if="message.name == 'Admin'" class="text-danger">[{{ message.name }}]</span>
+                                <span v-else class="text-info">[{{ message.name }}]</span>
+                                <span class="pl-2">{{message.message}}</span>
+                                <span class="text-secondary time">{{message.timestamp}}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <CreateMessage :name="name" />
                     </div>
                 </div>
             </div>
-            <div class="card-action">
-                <CreateMessage :name="name" />
-            </div>
+
+            
         </div>
+
     </div>
 </template>
 
@@ -29,40 +43,54 @@ import moment from 'moment';
 
 export default {
     name: "Chat",
-    // props: ['name'],
     components: {
         CreateMessage
     },
     data() {
         return {
             messages: [],
-            name: "ihsan"
+            name: "",
+            userChat: {},
+            user_uid: null
         }
     },
     firestore() {
         let user = fb.auth().currentUser;
         return {
-            messages : db.collection('messages').where("user_id", "==", user.uid).orderBy('timestamp')
+            messages : db.collection('messages').where("user_id", "==", user.uid).orderBy('timestamp'),
+            userChat : db.collection('profiles').doc(user.uid)
         }
     },
-    // created() {
-    //     let user = fb.auth().currentUser;
-    //     let ref = db.collection('messages').where("user_id", "==", user.uid).orderBy('timestamp');
+    methods: {
+        startChat() {
+            const data = {
+                isMessage : true
+            }
 
-    //     ref.onSnapshot(snapshot => {
-    //         snapshot.docChanges().forEach(change => {
-    //             if (change.type = "added") {
-    //                 let doc = change.doc;
-    //                 this.messages.push({
-    //                     id: doc.id,
-    //                     name: doc.data().name,
-    //                     message: doc.data().message,
-    //                     timestamp: moment(doc.data().timestamp).format('LTS')
-    //                 })
-    //             }
-    //         })
-    //     })
-    // }
+            this.$firestore.userChat.update(data)
+            .then(()=> console.log('sukses'))
+            .catch((err)=> console.log(err));
+            }
+    },
+    created() {
+        let user = fb.auth().currentUser;
+        if (user) {
+        this.user_uid = user.uid
+        let docRef = db.collection("profiles").doc(user.uid);
+        
+            docRef.get().then((doc) => {
+                if (doc.exists) {
+                    this.name = doc.data().name
+                } else {
+                    console.log("No such document!");
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+        } 
+
+
+    }
 }
 </script>
 
