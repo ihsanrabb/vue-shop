7f66fc86755df706b0a12ac1b430ed9ac3e926ed
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-07 22:41:36 +0700

    change delete to 100 message

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ exports.onMessageCreate = functions.firestore
         ref.onSnapshot(snapshot => {
             i = 0;
             size = snapshot.size;
-            sizeToDelete = size - 10;
+            sizeToDelete = size - 100;
             console.log('Messages Count: ' + size);
             
             snapshot.forEach((doc) => {
