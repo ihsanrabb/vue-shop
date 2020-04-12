c3e71e6bf6d205721988f1d28f62cb2453ac7a0d
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-12 22:00:01 +0700

    add form validator

--- src/views/Pembayaran.vue
+++ src/views/Pembayaran.vue
@@ -9,46 +9,79 @@
             <form>
                 <div class="form-group">
                     <label for="nama">Nama Lengkap</label>
-                    <input type="text" v-model="formData.nama" class="form-control" id="nama" placeholder="Nama Lengkap">
+                    <input 
+                        type="text" 
+                        v-model.trim="$v.formData.nama.$model" 
+                        class="form-control"
+                        :class="{invalid: $v.formData.nama.$error}" 
+                        id="nama" 
+                        placeholder="Nama Lengkap">
+                    <small class="form-text" v-if="$v.formData.nama.$error">Nama harus diisi</small>
                 </div>
                 <div class="form-group">
                     <label for="nama">Email</label>
-                    <input type="email" v-model="formData.email" class="form-control" id="email" placeholder="Email">
+                    <input 
+                        type="email"
+                        v-model.trim="$v.formData.email.$model" 
+                        class="form-control"
+                        :class="{invalid: $v.formData.email.$error}" 
+                        id="email" 
+                        placeholder="Email">
+                    <small class="form-text" v-if="$v.formData.email.$error">Pastikan alamat email yang anda masukkan benar</small>    
                 </div>
                 <div class="form-group">
                     <label for="telepon">No. Telepon</label>
-                    <input type="text" v-model="formData.noTelp" class="form-control" id="telepon" placeholder="No. Telepon">
+                    <input 
+                        type="text" 
+                        v-model.trim="$v.formData.noTelp.$model" 
+                        class="form-control"
+                        :class="{invalid: $v.formData.noTelp.$error}"  
+                        id="telepon" 
+                        placeholder="No. Telepon">
+                    <small class="form-text" v-if="$v.formData.noTelp.$error">Nomer Telepon harus diisi</small>
                 </div>
                 <div class="form-group">
                     <label for="alamat">Alamat</label>
-                    <textarea class="form-control" v-model="formData.alamat" id="alamat" rows="3"></textarea>
+                    <textarea 
+                        class="form-control" 
+                        v-model.trim="$v.formData.alamat.$model"
+                        :class="{invalid: $v.formData.alamat.$error}"   
+                        id="alamat" rows="3">
+                    </textarea>
+                    <small class="form-text" v-if="$v.formData.alamat.$error">Alamat harus diisi</small>
                 </div>
                  <div class="form-group">
                     <label for="provinsi">Provinsi</label>
-                    <select id="provinsi" class="form-control" v-model="selectedProvinsi" @change="onChangeProv($event)">
-                        <option v-for="option in optionsProvinsi" :value="option.id">{{option.nama}}</option>
-                        <option selected>--Pilih Provinsi--</option>
+                    <select id="provinsi" class="form-control" v-model="selectedProvinsi" >
+                        <option @click="getKota" v-for="(option, index) in optionsProvinsi" :value="option.id" :key="index">{{option.nama}}</option>
+                        
                     </select>
-                    
                 </div>
                  <div class="form-group">
                     <label for="provinsi">Kota</label>
                     <select id="kota" class="form-control" v-model="selectedKota" @change="onChangeKota($event)">
-                        <option v-for="option in optionsKota" :value="option.id">{{option.nama}}</option>
+                        <option v-for="(option,index) in optionsKota" :value="option.id" :key="index">{{option.nama}}</option>
                         <option selected>--Pilih Kota--</option>
                     </select>
                 </div>
                  <div class="form-group">
                     <label for="provinsi">Kecamatan</label>
                     <select id="kota" class="form-control" v-model="selectedKecamatan" >
-                        <option v-for="option in optionsKecamatan" :value="option.id">{{option.nama}}</option>
+                        <option v-for="(option, index) in optionsKecamatan" :value="option.id" :key="index">{{option.nama}}</option>
                         <option selected>--Pilih Kecamatan--</option>
                     </select>
                 </div>
 
                 <div class="form-group">
                     <label for="pos">Kode Pos</label>
-                    <input type="text" v-model="formData.kodePos" class="form-control" id="pos" placeholder="Kode pos">
+                    <input 
+                        type="text" 
+                        v-model="$v.formData.kodePos.$model" 
+                        class="form-control"
+                        :class="{invalid: $v.formData.kodePos.$error}"    
+                        id="pos" 
+                        placeholder="Kode pos">
+                    <small class="form-text" v-if="$v.formData.kodePos.$error">Kode Pos harus diisi</small>
                 </div>
 
                  <button type="button" class="btn btn-primary" @click="toReview">Lanjutkan</button>
@@ -59,6 +92,7 @@
 </template>
 
 <script>
+import { required, email, numeric } from 'vuelidate/lib/validators'
 import axios from 'axios'
 
 export default {
@@ -72,6 +106,7 @@ export default {
                 alamat: '',
                 kodePos: ''
             },
+            email: '',
             selectedProvinsi: '',
             optionsProvinsi: [],
             selectedKota: '',
@@ -80,14 +115,41 @@ export default {
             optionsKecamatan: []
         }
     },
+    validations: {
+        formData: {
+            nama: {
+                required
+            },
+            email: {
+                required,
+                email
+            },
+            noTelp: {
+                required,
+                numeric
+            },
+            alamat: {
+                required
+            },
+            kodePos: {
+                required,
+                numeric
+            }
+        }
+        
+    },
     methods: {
+        getKota() {
+            console.log('helo kota')
+        },
         onChangeProv(event) {
             let idProvinsi = event.target.value
-            axios.get(`http://dev.farizdotid.com/api/daerahindonesia/provinsi/${idProvinsi}/kabupaten`)
-            .then(res => {
-                this.optionsKota = res.data.kabupatens
-            })
-            .catch(err => console.log(err))
+            console.log('isi prov', event.target.value)
+            // axios.get(`http://dev.farizdotid.com/api/daerahindonesia/provinsi/${idProvinsi}/kabupaten`)
+            // .then(res => {
+            //     this.optionsKota = res.data.kabupatens
+            // })
+            // .catch(err => console.log(err))
         },
         onChangeKota(event) {
             let idKabupaten = event.target.value
@@ -97,16 +159,24 @@ export default {
             })
             .catch(err => console.log(err))
         },
-         toReview() {
-             const data = {
-                 nama: this.formData.nama,
-                 email: this.formData.email,
-                 noTelp: this.formData.noTelp,
-                 alamat: this.formData.alamat,
-                 kodePos: this.formData.kodePos
+        toReview() {
+             console.log('submit!')
+             this.$v.$touch()
+             if (this.$v.$invalid) {
+                 console.log('error submit')
+             } else {
+                 console.log('sukses submit')
+                    const data = {
+                        nama: this.formData.nama,
+                        email: this.formData.email,
+                        noTelp: this.formData.noTelp,
+                        alamat: this.formData.alamat,
+                        kodePos: this.formData.kodePos
+                    }
+                    window.localStorage.setItem('pengirimanHolder' , JSON.stringify(data))
+                    this.$router.push({path: '/reviewPembayaran'})
              }
-             window.localStorage.setItem('pengirimanHolder' , JSON.stringify(data))
-             this.$router.push({path: '/reviewPembayaran'})
+          
          }
     },
     mounted() {
