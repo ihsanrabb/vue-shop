9628d5fbc78e1e96fb96f02c0d3487881641612f
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-13 21:42:35 +0700

    create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hajj-footer">
+        <p>Copyright 2020. All Rights Reserved</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.hajj-footer {
+    margin-top: 3rem;
+    height: 100px;
+    background-color: #eef8f6;
+    width: 100%;
+    p {
+        font-size: 18px;
+        display: inline-block;
+        vertical-align: middle;
+        line-height: 100px; 
+    }
+}
+</style>
