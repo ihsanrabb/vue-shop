9b1e7fe129a46fc46776791b9d67a683fadd88b3
ihsanrabb <ihsanrabbani25@gmail.com>
2020-04-14 22:49:24 +0700

    add collapse for bank

--- src/views/ReviewPembayaran.vue
+++ src/views/ReviewPembayaran.vue
@@ -12,6 +12,64 @@
                 <p><span>NOMER ORDER ANDA</span> : {{uniqueOrder}}</p>
             </div>
             
+            <label>Pilih Kurir Pengiriman</label>
+            <select class="form-control">
+                <option>JNE</option>
+                <option>POS</option>
+                <option>TIKI</option>
+            </select>
+
+            <div class="accordion mx-auto mt-5 mb-5" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Collapsible Group Item #1
+                    </button>
+                </h2>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Collapsible Group Item #2
+                    </button>
+                </h2>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Collapsible Group Item #3
+                    </button>
+                </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+                </div>
+            </div>
+            </div>
+
+
+
+
+
+
             <div class="bukti-bayar mt-4">
                 <h5>BUKTI PEMBAYARAN</h5>
                 <div class="input-group mb-3">
@@ -42,13 +100,29 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+
+            
+
+
+
+
+
+
+
             
             <button 
                     @click="saveOrder()" 
                     type="button" 
                     class="btn btn-primary"
                     >
-                    Konfirmasi</button>
+                    Konfirmasi
+        
+            </button>
+
         </div>
         
         <div v-else>
@@ -56,7 +130,7 @@
             <p>Sedang menyimpan data</p>
         </div>
         
-        <Footer style="position: fixed; bottom: 0" />
+        <Footer />
     </div>
 </template>
 
@@ -240,9 +314,7 @@ export default {
         this.sentHolderData = JSON.parse(window.localStorage.getItem('pengirimanHolder'));
         this.cartData = JSON.parse(window.localStorage.getItem('cart'));
         this.totalPrice = JSON.parse(window.localStorage.getItem('priceHolder'));
-        this.uniqueOrder = this.generateId(3,5)
-     
-        
+        this.uniqueOrder = this.generateId(3,5)    
     }
 }
 </script>
