986476b43aa99bd1236ae86ccc1ab9e8945199d5
ihsanrabb <ihsanrabbani25@gmail.com>
2020-05-03 23:15:12 +0700

    implement kelola pembeli

--- src/sections/AkunPembeli.vue
+++ src/sections/AkunPembeli.vue
@@ -24,7 +24,7 @@
                 </thead>
 
                 <tbody>
-                <tr v-for="(pembeli, index) in paraPembeli" :key="index">
+                <tr v-for="(pembeli, index) in pembelis" :key="index">
                     <td>
                         {{pembeli.name}}
                     </td>
@@ -38,13 +38,64 @@
                     </td>
 
                     <td>
-                    <button class="btn btn-primary" >Edit</button>
+                    <button class="btn btn-primary" @click="detailPembeli(pembeli)">Detail</button>
                     </td>
 
                 </tr>
                 </tbody>
             </table>
         </div>
+
+        <!-- modal akun penjual -->
+        <div class="modal" tabindex="-1" role="dialog" id="akun-pembeli">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Detail Akun Pembeli</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" v-if="pembeliDetail">
+                    <div class="form-group">
+                        <input type="email" class="form-control" v-model="pembeliDetail.id" readonly>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="pembeliDetail.name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" v-model="pembeliDetail.email">
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control" v-model="pembeliDetail.phone" placeholder="Nomer Telepon">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="pembeliDetail.address" placeholder="Alamat">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="pembeliDetail.postcode" placeholder="Kode pos">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" v-model="pembeliDetail.status" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-danger" @click="nonaktifPembeli">Non Aktif</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-success" @click="aktifPembeli">Aktif</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" @click="updatePembeli">Simpan</button>
+                </div>
+                </div>
+            </div>
+        </div>
+        <!-- end modal akun penjual -->
+
     
     </div>
 </template>
@@ -56,12 +107,57 @@ export default {
     name: "akunPembeli",
     data() {
         return {
-            paraPembeli : []
+            pembelis : [],
+            pembeliDetail: null
         }
     },
     firestore() {
         return {
-            paraPembeli : db.collection('profiles').where("userType", "==", "pembeli")
+            pembelis : db.collection('profiles').where("userType", "==", "pembeli"),
+            pembeliData : db.collection('profiles')
+        }
+    },
+    methods: {
+        detailPembeli(pembeli) {
+            this.pembeliDetail = pembeli
+            $("#akun-pembeli").modal("show")
+        },
+        updatePembeli() {
+            this.$firestore.pembeliData.doc(this.pembeliDetail.id).update(this.pembeliDetail)
+                .then(() => {
+                    $("#akun-pembeli").modal("hide")
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'update successfully'
+                    })
+                })
+                .catch((err) => console.log(err))
+        },
+        nonaktifPembeli() {
+            this.$firestore.pembeliData.doc(this.pembeliDetail.id).update({
+                    status: "non_aktif"
+                })
+                .then(() => {
+                    $("#akun-pembeli").modal("hide")
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'update successfully'
+                    })
+                })
+                .catch((err) => console.log(err))
+        },
+        aktifPembeli() {
+            this.$firestore.pembeliData.doc(this.pembeliDetail.id).update({
+                    status: "aktif"
+                })
+                .then(() => {
+                    $("#akun-pembeli").modal("hide")
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'update successfully'
+                    })
+                })
+                .catch((err) => console.log(err))
         }
     }
 }
