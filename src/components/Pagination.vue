05ccd9b1e40171e8f1043de311486df46d282c37
ihsanrabb <ihsanrabbani25@gmail.com>
2020-05-04 21:43:06 +0700

    add disable button condition

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -5,9 +5,9 @@
         <span v-if="showNextLink()" class="pagination-btn" @click="updatePage(currentPage + 1)"> > </span> -->
         <nav aria-label="Page navigation">
             <ul class="pagination justify-content-center">
-                <li class="page-item"><a class="page-link pagination-btn" @click="updatePage(currentPage - 1)">Previous</a></li>
+                <li class="page-item" :class="{disabled: !showPreviousLink()}"><a class="page-link pagination-btn" @click="updatePage(currentPage - 1)">Previous</a></li>
                 <li class="page-item disabled"><a class="page-link">{{ currentPage + 1 }} of {{ totalPages() }}</a></li>
-                <li class="page-item"><a class="page-link pagination-btn" @click="updatePage(currentPage + 1)">Next</a></li>
+                <li class="page-item" :class="{disabled: !showNextLink()}"><a class="page-link pagination-btn" @click="updatePage(currentPage + 1)">Next</a></li>
             </ul>
         </nav>
     </div>
@@ -36,12 +36,9 @@ export default {
 
 <style lang="scss" scoped>
 .pagination-wrapper {
-    padding-top: 0.7em;
+    padding-top: 1em;
     .pagination-btn {
         color: blue;
-        // padding-left: 0.7em;
-        // padding-right: 0.7em;
-        // font-size: 20px;
         cursor: pointer;
     }
 }
