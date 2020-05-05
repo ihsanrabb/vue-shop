535c8b0330cc5a8b2c84c1ba8baf3ad46958ead3
ihsan <ihsanrabbani25@gmail.com>
2020-05-05 15:12:54 +0700

    change admin login logic

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -6,16 +6,16 @@
             <p class="subtitle text-center">
                 Hajj Shop
             </p>
-            <div v-if="error" class="alert alert-danger" role="alert">
-                Hanya Admin yang bisa Login!
+            <div v-if="errorMsg" class="alert alert-danger" role="alert">
+                {{errorMsg}}
             </div>
             <div class="form-group">
                 <input type="email" class="form-control" placeholder="Email Address" v-model="email">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password" v-model="password">
+                <input type="password" class="form-control" placeholder="Password" v-model="password" @keyup.enter="loginAdmin">
             </div>
-            <button type="submit" class="btn-login" @click="loginAdmin">Login</button>
+            <button type="submit" :disabled="isLoading" class="btn-login" @click="loginAdmin">{{btnText}}</button>
         </div>
 
     </div>
@@ -30,21 +30,29 @@ export default {
         return {
             email: "",
             password: "",
-            error: false
+            btnText: "Login",
+            isLoading: false,
+            errorMsg : ""
         }
     },
     methods: {
         loginAdmin() {
+            this.btnText = "Loading..."
+            this.isLoading = true
             fb.auth().signInWithEmailAndPassword(this.email, this.password)
             .then((res) => {
+                this.btnText = "Login"
+                this.isLoading = false
                 if (res.user.uid == 'KzW5BVel1oaG14WOJKO3zSCwwgF3') {
-                    this.$router.push('/adminPage')
+                    this.$router.push('/adminPage/informasi')
                 } else {
-                    this.error = true
+                    this.errorMsg = "Hanya Admin yang bisa Login!"
                 }
             })
-            .catch(function(error) {
-                console.log(error)
+            .catch((error) => {
+                this.btnText = "Login"
+                this.isLoading = false
+                this.errorMsg = "Pastikan email dan password yang anda masukkan sudah benar"
             });
         }
     }
