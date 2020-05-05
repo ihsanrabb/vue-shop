a8113d19d874012ff2b722b5fb7963b3bb8e0010
ihsan <ihsanrabbani25@gmail.com>
2020-05-05 15:12:38 +0700

    add end chat

--- src/components/CreateMessageAdmin.vue
+++ src/components/CreateMessageAdmin.vue
@@ -2,10 +2,19 @@
     <div class="container" style="margin-bottom: 30px">
         <form @submit.prevent="createMessage"> 
             <div class="form-group">
-                <input type="text" name="message" class="form-control" placeholder="Enter message ... " v-model="newMessage">
+                <div class="row">
+                    <div class="col-md-8">
+                        <input type="text" name="message" class="form-control" placeholder="Enter message ... " v-model="newMessage">
+                    </div>
+                    <div class="col-md-4">
+                        <button class="btn btn-danger mr-3" @click="endChat">Selesai</button>
+                        <button class="btn btn-success" type="submit" name="action">Submit</button>
+                    </div>
+                </div>
+                
                 <p class="text-danger" v-if="errorText">{{ errorText }}</p>
             </div>
-            <button class="btn btn-success" type="submit" name="action">Submit</button>
+            
         </form>
     </div>
 </template>
@@ -19,12 +28,18 @@ export default {
     data() {
         return {
             newMessage: null,
-            errorText: null
+            errorText: null,
+            chat: []
+        }
+    },
+    firestore() {
+        return {
+            userProfile : db.collection('profiles').doc(this.userChatId)
         }
     },
     methods: {
         createMessage() {
-             let user = fb.auth().currentUser;
+            let user = fb.auth().currentUser;
 
             if (this.newMessage) {
                 db.collection('messages').add({
@@ -40,6 +55,23 @@ export default {
             } else {
                 this.errorText = "A message must be fill";
             }
+        },
+        endChat() {
+            Swal.fire({
+                title: 'Apakah Anda yakin?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, Selesai!'
+            }).then((result) => {
+                if (result.value) {
+                    this.$firestore.userProfile.update({isMessage : false})
+                    .then(() => {
+                        this.$router.push('/adminPage/chatList')
+                    })      
+                }
+            })
         }
     }
 }
