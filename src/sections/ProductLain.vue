64725db64557511b9170fc20a5e5bacb773d06a7
ihsan <ihsanrabbani25@gmail.com>
2020-05-07 16:55:09 +0700

    create produk lainnya

---
+++ src/sections/ProductLain.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="products" id="product-lain">
+      <div class="container">  
+          <div class="row">
+              <div class="col-md-4" v-for="(product,index) in visibleProducts" :key="index">
+                  <div class="card product-item card-product mt-5">
+                    <img :src="getImage(product.images)" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="product-title">{{ product.name }}</h5>
+                            <h5 class="product-price">{{ product.price | currency('Rp') }}</h5>
+  
+                            <button @click="goToDetail(product.id)" class="btn btn-warning btn-detail">Detail</button>    
+ 
+                        </div>
+                    </div>
+              </div>
+
+          </div>
+
+          <pagination 
+            :products="products"
+            @page:update="updatePage"
+            :currentPage="currentPage"
+            :pageSize="pageSize"
+          />
+
+      </div>
+    
+  </div>
+</template>
+
+<script>
+import {fb,db} from '../firebase';
+import AddToCart from '../components/AddToCart' 
+import Pagination from '../components/Pagination'
+
+export default {
+  name: "ProductLain",
+  props: ['search'],
+  components: {
+    AddToCart,
+    Pagination
+  },
+  data() {
+    return {
+      products: [],
+      currentPage: 0,
+      pageSize: 12
+    }
+  },
+  firestore() {
+    return {
+        products: db.collection('products').where("productCategory", "==", "lain-lain")
+    }
+  },
+  methods: {
+    getImage(images) {
+      return images[0]
+    },
+    goToDetail(prodId) {
+      this.$router.push({name:'productDetail', query: {pId: prodId}})
+    },
+    updatePage(pageNumber) {
+      this.currentPage = pageNumber;
+    },
+  },
+  computed: {
+    visibleProducts() {
+      if(this.search) {
+        return this.products.filter((product) => {
+          return product.name.toLowerCase().match(this.search.toLowerCase())
+        })
+      } else {
+        return this.products.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize)
+        if (this.products.length == 0 && this.currentPage > 0) {
+          return this.updatePage(this.currentPage - 1)
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss" src="../styles/ProductList.scss">
