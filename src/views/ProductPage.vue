3f9b200ed2136403bc9792c07d7026477f60d7f3
ihsanrabb <ihsanrabbani25@gmail.com>
2020-05-07 22:50:22 +0700

    create rekomendasi

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -6,9 +6,21 @@
                 
             </div>
 
-            <h1 class="mt-5 mb-5">Produk Kami</h1>
+            
+
+            
             
             <div class="container">
+                
+            <div class="box-rekomendasi">
+                <h4>Bingung perlengkapan apa saja yang harus dipersiapkan?</h4>
+                <p>Yuk,cek beberapa rekomendasi perlengkapan Haji dan umroh dari Hajj Shop</p>
+                <button type="button" class="btn btn-primary mr-4" data-toggle="modal" data-target="#rekomendasi-muslim">Perlengkapan Muslim</button>
+                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#rekomendasi-muslimah">Perlengkapan Muslimah</button>
+            </div>    
+
+            <h1 class="mt-5 mb-5">Produk Kami</h1>
+
                 <div class="row">
                     <div class="col-md-9">
                         <div class="mb-5 float-left">
@@ -60,7 +72,64 @@
                     </div>
                 </div>
             </div>
-            
+
+            <!-- modal muslim -->
+            <div class="modal rekomendasi" tabindex="-1" role="dialog" id="rekomendasi-muslim">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Perlengkapan Muslim</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <ol>
+                            <li @click="searchRekomendasi('ihram')">Kain Ihram</li>
+                            <li @click="searchRekomendasi('sabuk')">Sabuk Haji</li>
+                            <li @click="searchRekomendasi('koko')">Baju Koko</li>
+                            <li @click="searchRekomendasi('sorban')">Sorban</li>
+                            <li @click="searchRekomendasi('pangsi')">Celana Pangsi</li>
+                            <li @click="searchRekomendasi('dalam ihram')">Celana Dalam Ihram</li>
+                            <li @click="searchRekomendasi('gamis')">Gamis Jubah Muslimah</li>
+                            <li @click="searchRekomendasi('handuk')">Handuk Kecil</li>
+                            <li @click="searchRekomendasi('quran')">Al-Quran</li>
+                            <li @click="searchRekomendasi('sandal')">Sandal</li>
+                        </ol>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end modal muslim -->
+
+            <!-- modal muslimah -->
+            <div class="modal rekomendasi" tabindex="-1" role="dialog" id="rekomendasi-muslimah">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Perlengkapan Muslimah</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <ol>
+                            <li @click="searchRekomendasi('khimar')">Khimar/Jilbab Panjang</li>
+                            <li @click="searchRekomendasi('mukena')">Mukena</li>
+                            <li @click="searchRekomendasi('inner')">Inner Pants</li>
+                            <li @click="searchRekomendasi('gamis')">Gamis Muslimah</li>
+                            <li @click="searchRekomendasi('kaos kaki')">Kaos Kaki</li>
+                            <li @click="searchRekomendasi('handuk')">Handuk Kecil</li>
+                            <li @click="searchRekomendasi('quran')">Al-Quran</li>
+                            <li @click="searchRekomendasi('sajadah')">Sajadah</li>
+                        </ol>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end modal muslimah -->
+
+
             <main class="page-content pt-2">
                 <router-view :search="this.search" />
             </main>
@@ -78,6 +147,12 @@ export default {
       return {
           filter: 'All',
           search: ""
+      }
+  },
+  methods: {
+      searchRekomendasi(keywoard) {
+          this.search = keywoard
+          $(".rekomendasi").modal("hide")
       }
   }
 };
