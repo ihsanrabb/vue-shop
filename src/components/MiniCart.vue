6a0b7e4280624bc7890e66b82c1a8ec7fbd4c23a
ihsan rabbs <ihsanrabbani25@gmail.com>
2020-06-14 07:16:33 +0700

    check if transaksi exist

--- src/components/MiniCart.vue
+++ src/components/MiniCart.vue
@@ -12,6 +12,9 @@
             </button>
           </div>
           <div class="modal-body">
+            <div class="alert alert-success" role="alert" v-if="isTransaksi">
+              Jangan ada transaksi tertunda diantara kita! Ayo selesaikan pembayaran kamu terlebih dahulu
+            </div>
             <table class="table table-image">
               <thead>
                 <tr>
@@ -19,18 +22,18 @@
                   <th scope="col">Product</th>
                   <th scope="col">Price</th>
                   <th scope="col">Qty</th>
-                  <th scope="col">Actions</th>
+                  <th scope="col" v-if="!isTransaksi">Actions</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="item in this.$store.state.cart">
+                <tr v-for="(item,index) in this.$store.state.cart" :key="index">
                   <td class="w-25">
                     <img :src="item.productImage" class="img-fluid img-thumbnail" alt="Sheep">
                   </td>
                   <td>{{item.productName}}</td>
                   <td>{{item.productPrice | currency('Rp') }}</td>
                   <td>{{item.productQuantity}}</td>
-                  <td>
+                  <td v-if="!isTransaksi">
                     <span @click="$store.commit('removeFromCart', item)" class="btn btn-danger btn-sm">
                       <i class="fa fa-times"></i>
                     </span>
@@ -40,8 +43,11 @@
             </table> 
           </div>
           <div class="modal-footer border-top-0 d-flex justify-content-end">
-            <button type="button" class="btn btn-info" data-dismiss="modal">Lanjutkan Belanja</button>
-            <button type="button" class="btn btn-success" @click="checkout">Checkout</button>
+              <!-- <p v-if="isTransaksi">Jangan ada transaksi tertunda diantara kita!</p> -->
+              <button type="button" class="btn btn-success" @click="lanjutPembayaran" v-if="isTransaksi">Lanjutkan Pembayaran</button>
+             
+              <button type="button" class="btn btn-info" data-dismiss="modal" v-if="!isTransaksi">Lanjutkan Belanja</button>
+              <button type="button" class="btn btn-success" @click="checkout" v-if="!isTransaksi">Checkout</button>
           </div>
         </div>
       </div>
@@ -56,12 +62,36 @@
 <script>
 export default {
   name: "MiniCart",
+  data() {
+    return {
+      isTransaksi: false
+    }
+  },
   methods: {
     checkout() {
       $('#miniCart').modal('hide')
       this.$router.push('/checkout')
       
+    },
+    lanjutPembayaran() {
+      $('#miniCart').modal('hide')
+      this.$router.push('/reviewPembayaran')
     }
+  },
+  created() {
+    let pembayaran_tgl = JSON.parse(window.localStorage.getItem('pembayaranTgl'));
+    if(pembayaran_tgl) {
+      this.isTransaksi = true
+      var tempDate = pembayaran_tgl.datetime_pembayaran
+      var nowDate = new Date().toLocaleString()
+      if(nowDate >= tempDate) {
+        this.isTransaksi = false
+        localStorage.removeItem("cart");
+        localStorage.removeItem("shipmentHolder");
+        localStorage.removeItem("pengirimanHolder");    
+        localStorage.removeItem("pembayaranTgl");   
+      }
+    } 
   }
 };
 </script>
